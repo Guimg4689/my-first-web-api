fc2dd2e08cc59a473a0fd14be59bdbc3c984c71a
Guimg4689 <guilherme.martins20@hotmail.com>
2024-04-30 13:29:03 -0300

    Adicionando Exception Handlers

--- src/main/java/dio/web/api/repository/UsuarioRepository.java
+++ src/main/java/dio/web/api/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package dio.web.api.repository;
 
+import dio.web.api.handler.BusinessException;
 import dio.web.api.model.Usuario;
 import org.springframework.stereotype.Repository;
 
@@ -9,9 +10,16 @@ import java.util.List;
 @Repository
 public class UsuarioRepository {
     public void save(Usuario usuario){
-        System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+        if(usuario.getLogin() == null){
+            throw new BusinessException("O Campo Login e obrigatorio");
+        }
+        if(usuario.getId() ==null) {
+            System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+        }else{
+            System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+        }
         System.out.println(usuario);
-    }
+        }
     public void update(Usuario usuario){
         System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
         System.out.println(usuario);
